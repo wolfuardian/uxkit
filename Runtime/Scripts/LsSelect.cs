ae9116f2a82b949c6632d277a28e6d408219dc13
Eos Wolfuardian <wolfuardian@gmail.com>
2025-07-07 01:59:48 +0800

    feat: add ls-select component

---
+++ Runtime/Scripts/LsSelect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Eos.UxKit
+{
+    public class LsSelect : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _onSelected = new UnityEvent();
+        [SerializeField] private UnityEvent _onDeselected = new UnityEvent();
+
+        private bool _isSelected;
+
+        public void Toggle()
+        {
+            _isSelected = !_isSelected;
+            if (_isSelected)
+            {
+                Selecting();
+            }
+            else
+            {
+                Deselecting();
+            }
+        }
+
+        public void Selecting()
+        {
+            _isSelected = true;
+            _onSelected.Invoke();
+        }
+
+        public void Deselecting()
+        {
+            _isSelected = false;
+            _onDeselected.Invoke();
+        }
+    }
+}
