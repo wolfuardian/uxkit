2eefa09ab00dbabb905c3e575c9399545a11921c
Eos Wolfuardian <wolfuardian@gmail.com>
2025-07-07 00:21:31 +0800

    feat: add go switcher

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,11 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/).
 
-## [0.1.0-preview] - 2025-07-06
+## [0.1.1-alpha] - 2025-07-06
+### Added
+- Add LsSwitchGameObject.cs
+
+## [0.1.0-alpha] - 2025-07-06
 ### Added
 - Project initialized.
 - Core package structure created.
---
+++ Runtime/Scripts/LsSwitchGameObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eos.UxKit
+{
+    public class LsSwitchGameObject : MonoBehaviour
+    {
+        [SerializeField] private Settings _settings = new Settings();
+
+        public Settings settings
+        {
+            get
+            {
+                return _settings;
+            }
+        }
+
+        public void Switch(int index)
+        {
+            settings.gameObjects.ForEach(go => go.SetActive(false));
+            if (index >= 0 && index < settings.gameObjects.Count)
+            {
+                settings.gameObjects[index].SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning("Index out of range: " + index);
+            }
+        }
+
+        [System.Serializable]
+        public class Settings
+        {
+            public List<GameObject> gameObjects = new List<GameObject>();
+        }
+    }
+}
